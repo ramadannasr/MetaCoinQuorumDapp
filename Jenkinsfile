6ba848adeb32cf51d3ee3f254b812257504317b5
EMEINT\ramadan.nasr <ramadan.nasr@emeint.net>
2022-10-10 07:02:31 +0200

    update Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,22 +3,50 @@ pipeline {
     tools{
     nodejs '16.17.0'
     }
+       environment {
+        JENKINS_UID = sh(returnStdout: true, script: "id -u ${USER}").trim()
+    }
     stages {
         stage('Build SimpleStorage Quorum Dapp') {
             steps {
                     ansiColor('css') {
-                        bat 'npm install'
-                        bat 'node_modules/.bin/truffle compile'
+                        sh 'npm install'
+                        sh 'node_modules/.bin/truffle compile'
                     }
             }
         }
-        stage('Test') { 
+
+        stage('Apply Config') {
             steps {
-                    ansiColor('vga') {
-                        bat 'npm install'
-                        bat 'node_modules/.bin/truffle test'
-                    }
+                sh 'bash -c "chown -R ${JENKINS_UID} ."'
+                sh 'rm "truffle-config.js"'
+                sh 'cp "nodeConfigFiles/truffle-config.js" .'
+
+
+
             }
+        }
+        stage('Test') {
+            steps {
+                sh 'node_modules/.bin/truffle test'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+
+                sh 'node_modules/.bin/truffle migrate'
+
+            }
+        }
+    }
+
+        post {
+        aborted {
+            error "Aborted, exiting now"
+        }
+        failure {
+            error "Failed, exiting now"
         }
     }
 }
---
+++ nodeConfigFiles/truffle-config.js
@@ -0,0 +1,67 @@
+
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache, geth, or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+
+   
+
+       Homer:  {
+          host: ["localhost"],
+          port: 22000,
+          network_id: "*", // Match any network id
+          gasPrice: 0,
+          gas: 4500000,
+          type: "quorum"
+        },
+
+       Charles:  {
+          host: ["localhost"],
+          port: 22001,
+          network_id: "*", // Match any network id
+          gasPrice: 0,
+          gas: 4500000,
+          type: "quorum"
+        },
+
+       Victor:  {
+          host: ["localhost"],
+          port: 22002,
+          network_id: "*", // Match any network id
+          gasPrice: 0,
+          gas: 4500000,
+          type: "quorum"
+        },
+
+
+
+
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.13",      // Fetch exact version from solc-bin
+    }
+  }
+};
